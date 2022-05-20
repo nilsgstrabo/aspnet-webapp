e65079917240582816676e22a63add083d039136
Nils Gustav Stråbø <nst@equinor.com>
2022-05-20 14:58:39 +0200

    another test

--- .github/workflows/scripts/create-db.ps1
+++ .github/workflows/scripts/create-db.ps1
@@ -3,6 +3,6 @@ Import-Module Az.Accounts -MinimumVersion 2.2.0
 
 $targetSqlServerFQDN = "$(az sql server show -n sql-radix-cost-allocation-dev -g cost-allocation | jq -r .fullyQualifiedDomainName)"
 
-$access_token = (Get-AzAccessToken -ResourceUrl https://database.windows.net).Token
+$access_token = (Get-AzAccessToken -ResourceUrl "https://database.windows.net").Token
 
 Invoke-Sqlcmd -InputFile ${env:GITHUB_WORKSPACE}/.github/workflows/scripts/sql/init.sql -AccessToken $access_token -ServerInstance $targetSqlServerFQDN -Database sqldb-radix-cost-allocation
