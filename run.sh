1464cff4518c43cee551122a378c2312134df5f0
Nils Gustav Stråbø <nst@equinor.com>
2023-11-09 08:34:25 +0100

    cddfs

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+declare -i x=0
 echo "checking if directory /usr/share/aasx has files"
 echo "$(date -Ins): starting"
 
-while [ $(ls /usr/share/aasx | wc -l) -eq 0 ]
+while [ $(ls /usr/share/aasx | wc -l) -eq 0 -a $x -lt 200  ]
 do
     echo "$(date -Ins): still empty"
-    
+    ((x=x+1))
 done
 
 echo "$(date -Ins): $(ls /usr/share/aasx | wc -l) file(s) found"
