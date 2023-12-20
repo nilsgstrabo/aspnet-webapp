b27075b30b29ab9ed5a5ccb04922daf61d25aa2b
Nils Gustav Stråbø <nst@equinor.com>
2023-12-20 13:36:04 +0100

    gfdgdf

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace aspnet_webapp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                
                 .ConfigureAppConfiguration(config => 
                 {
                     config.AddAzureKeyVault(
@@ -26,6 +27,7 @@ namespace aspnet_webapp
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureKestrel(k=>k.Limits.MaxRequestBodySize=104_857_600); // 100MB
                     webBuilder.UseStartup<Startup>();
                 });
     }
