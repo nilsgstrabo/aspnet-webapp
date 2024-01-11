cd38f4be9c1e4dcd626f15fb19a2e8db218a46b7
Nils Gustav Stråbø <nst@equinor.com>
2024-01-11 15:46:46 +0100

    add megafile

--- Controllers/VideoController.cs
+++ Controllers/VideoController.cs
@@ -13,9 +13,91 @@ using aspnet_webapp.Services;
 using System.Linq;
 using Microsoft.AspNetCore.Routing;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+using System;
+using Microsoft.Extensions.Caching.Memory;
+using System.Text;
 
 namespace aspnet_webapp.Controllers
 {
+
+    public class MegaStream : Stream
+    {
+        private readonly long size;
+        private long pos;
+        private readonly Stream data;
+
+        public MegaStream(long size)
+        {
+            data=new MemoryStream(Encoding.ASCII.GetBytes("this_is_the_content_of_the_megafile"));
+            this.size=size;
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => false;
+
+        public override long Length => size;
+
+        public override long Position { get => pos; set => pos=value; }
+
+        public override void Flush()
+        {
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (pos>=size) {
+                return 0;
+            }
+
+            if (data.Position>=data.Length) {
+                data.Position=0;
+            }
+
+            count = pos+count>size ? Convert.ToInt32(size-pos) : count;
+            var bytesRead=data.Read(buffer,offset,count);
+            
+            pos+=bytesRead;
+
+            return bytesRead;
+            // if (pos>=size) {
+            //     return 0;
+            // }
+
+            // int cnt = count>65_000 ? 65_000 : count;
+            // int bytesRead = 0;
+            // for (int i = 0; i < cnt; i++)
+            // {
+            //     if (pos+i>=size) {
+            //         break;
+            //     }
+            //     buffer[i+offset]=Convert.ToByte((pos+i) % 26 +65);
+            //     bytesRead++;
+            // }
+
+            // this.pos+=bytesRead;
+            // return bytesRead;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
     [ApiController]
     [Route("api/[controller]")]
     public class VideoController : ControllerBase
@@ -34,10 +116,28 @@ namespace aspnet_webapp.Controllers
         [HttpGet()]
         public IActionResult GetVideos()
         {
-            
+            var s=new FileStream("",FileMode.OpenOrCreate);
             return this.Ok();
         }
 
+
+        [HttpGet("megafile")]
+        public IActionResult GetMegaFile(string name)
+        {
+            long filesize=1_000_000;
+            try
+            {
+                var s=Convert.ToInt64(Environment.GetEnvironmentVariable("MEGA_FILE_SIZE"));
+                filesize = s >0 ? s : filesize;
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(ex, "failed to get mega file size, using default 1GB");
+            }
+            _logger.LogInformation("Stream megafile");
+            return this.File(new MegaStream(filesize),"text/plain");
+           
+        }
 
         [HttpGet("{name}")]
         public IActionResult GetVideo(string name)
