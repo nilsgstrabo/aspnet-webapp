6fa55d2a77e34b7d3efccd9ca7fe7eccaf439aab
Nils Gustav Stråbø <nst@equinor.com>
2024-03-15 08:51:35 +0100

    add simple

---
+++ simpleweb/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	handler := gin.New()
+	handler.RemoveExtraSlash = true
+	handler.GET("/", func(ctx *gin.Context) {
+		fmt.Println("")
+		fmt.Printf("Remote addr: %s\n", net.ParseIP(ctx.Request.RemoteAddr))
+		fmt.Println("")
+		for k, v := range ctx.Request.Header {
+			fmt.Printf("%q: %v\n", k, v)
+		}
+		ctx.Status(http.StatusOK)
+	})
+
+	if err := http.ListenAndServe(":9001", handler); err != nil {
+		panic(err)
+	}
+}
