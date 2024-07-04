ee4cbe3c7fe2e4957a4199eaf176ca9e9bbeeda5
Nils Gustav Stråbø <nst@equinor.com>
2024-07-04 14:03:52 +0200

    dfgd

--- simpledotnet/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-
-builder.WebHost.ConfigureKestrel(k=>k.ListenAnyIP(8080));
-
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
