0659df4902a86393a63d93399ed6178de0140430
Nils Gustav Stråbø <nst@equinor.com>
2024-09-20 09:45:14 +0200

    fsdff

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ namespace aspnet_webapp
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.ConfigureKestrel(k=>{
-                        k.Limits.MaxRequestBodySize=1_000_000_000;// 1Gi
+                        k.Limits.MaxRequestBodySize=2_000_000_000;// 1Gi
                         k.Limits.MaxResponseBufferSize=5_000_000; // 5MI
                     }); 
                     webBuilder.UseStartup<Startup>();
