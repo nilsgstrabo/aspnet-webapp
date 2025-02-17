faa0c59667e51f5646308d182a5d007db2a5f087
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-02-17 15:23:22 +0100

    dfgdfg

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 declare -i x=0
-echo "checking if directory /usr/share/aasx has files"
+echo "checking if directory /mnt/videos has files"
 echo "$(date -Ins): starting"
 
-while [ $(ls /usr/share/aasx | wc -l) -eq 0 -a $x -lt 200  ]
+while [ $(ls /mnt/videos | wc -l) -eq 0 -a $x -lt 200  ]
 do
     echo "$(date -Ins): still empty"
     ((x=x+1))
 done
 
-echo "$(date -Ins): $(ls /usr/share/aasx | wc -l) file(s) found"
+echo "$(date -Ins): $(ls /mnt/videos | wc -l) file(s) found"
 
 echo "starting server"
 
