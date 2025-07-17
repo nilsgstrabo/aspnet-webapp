2cf0fddfd9302cea53999a364ba1e3c813ab1ac3
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-07-17 08:42:15 +0200

    fix: return 84

--- simpleweb/main.go
+++ simpleweb/main.go
@@ -227,7 +227,7 @@ func runServer(cmd *cobra.Command, args []string) error {
 			sleep = v
 		}
 		time.Sleep(time.Duration(sleep) * time.Second)
-		ctx.String(200, "commit #83")
+		ctx.String(200, "commit #84")
 		// Sleep between 0 and 1000 ms
 
 		// ctx.Status(http.StatusOK)
