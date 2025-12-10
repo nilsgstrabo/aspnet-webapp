8ec4bf79bfdc8be52ec783d26a968041e695f469
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-12-10 09:36:14 +0100

    dfsf

--- Startup.cs
+++ Startup.cs
@@ -79,7 +79,7 @@ namespace aspnet_webapp
             app.Use(async (ctx, next) => {
                 var logfactory = app.ApplicationServices.GetService<ILoggerFactory>();
                 var logger=logfactory.CreateLogger("middleware");
-                foreach (var h in ctx.Request.Headers.Where(h=>h.Key.StartsWith("X-")).AsEnumerable()) //.Where(h=>h.Key.StartsWith("X-Custom") || h.Key.ToLower().StartsWith("auth"))
+                foreach (var h in ctx.Request.Headers.Where(h=>h.Key.StartsWith("")).AsEnumerable()) //.Where(h=>h.Key.StartsWith("X-Custom") || h.Key.ToLower().StartsWith("auth"))
                 {
                     var value = h.Value.FirstOrDefault() ?? "";
                     logger.LogInformation("{0}:{1}", h.Key, value.Substring(0,value.Length>20 ? 20 : value.Length));
