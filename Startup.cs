0851b0c58be62fea557299948bedc2d323635dfb
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2025-12-11 08:48:15 +0100

    bvbcv

--- Startup.cs
+++ Startup.cs
@@ -70,6 +70,7 @@ namespace aspnet_webapp
             else
             {
                 app.UseExceptionHandler("/Error");
+                app.UseHsts();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             }
 
