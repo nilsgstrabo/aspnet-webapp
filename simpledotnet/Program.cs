a7c686ce2e52bc27bc898cb5bb3b091e1c6d4134
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2026-04-20 10:19:41 +0200

    fghfgh

--- simpledotnet/Program.cs
+++ simpledotnet/Program.cs
@@ -5,10 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
-	options.ForwardedHeaders = ForwardedHeaders.None; //ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+	options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
 
 	// Trust Istio proxies in Radix.
-	// options.KnownIPNetworks.Add(new System.Net.IPNetwork(IPAddress.Parse("10.0.0.0"), 8));
+	options.KnownIPNetworks.Add(new System.Net.IPNetwork(IPAddress.Parse("10.0.0.0"), 8));
 });
 
 var app = builder.Build();
